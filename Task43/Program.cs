b5ac4fe11f7e116e6ade73c7e0be3cf3dbe6a105
AndreyF <f008f@mai;.ru>
2023-03-19 01:33:33 +0300

    Task43 fix

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -5,7 +5,7 @@
 // для начала нужно решить уравнение x=(b2-b1)/(k1-k2);
 
 
-double EnteringNumber(string name)
+double GetDoubleFromConsole(string name)
 {
     System.Console.Write($"Введите {name}: ");
     return Convert.ToDouble(Console.ReadLine());
@@ -13,10 +13,10 @@ double EnteringNumber(string name)
 
 void IntersectionPoint()
 {
-    double b1 = EnteringNumber("b1");
-    double k1 = EnteringNumber("k1");
-    double b2 = EnteringNumber("b2");
-    double k2 = EnteringNumber("k2");
+    double b1 = GetDoubleFromConsole("b1");
+    double k1 = GetDoubleFromConsole("k1");
+    double b2 = GetDoubleFromConsole("b2");
+    double k2 = GetDoubleFromConsole("k2");
     double xСoordinate = (b2-b1)/(k1-k2);
     double yСoordinate = k1 * xСoordinate + b1;
     System.Console.Write($"Координаты точки пересечения прямых: ({xСoordinate}; {yСoordinate}) ");
